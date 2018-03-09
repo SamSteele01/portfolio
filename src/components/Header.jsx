9f0863b4d0a18c1e8ea6ec656429ae516b4addfb
SamSteele01 <ssteele017@gmail.com>
2018-03-09 14:56:01 -0500

    Apply Headroom wrapper

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import logo from '../styles/Logo.png';
+import Headroom from 'react-headroom';
 
 export default class Header extends Component {
   constructor(props) {
@@ -13,13 +14,16 @@ export default class Header extends Component {
   // want to have a contact button
   render() {
     return(
-      <div key='a'>
-        <header className="App-header">
-          <div className="take-Up-Space"></div>
-          <img src={logo} className="App-logo" alt="logo" />
-          {/* <h1 className="App-title">Portfolio</h1> */}
-          <button className="contact-button" onClick={this.props.displayContact}>Contact</button>
-        </header>
+      <div className='header-wrapper'>
+        <Headroom disableInlineStyles={true}>
+            
+          <header className="App-header">
+            <div className="take-Up-Space"></div>
+            <img src={logo} className="App-logo" alt="logo" />
+            {/* <h1 className="App-title">Portfolio</h1> */}
+            <button className="contact-button" onClick={this.props.displayContact}>Contact</button>
+          </header>
+        </Headroom>
       </div>
     );
   }
