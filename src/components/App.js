0b6a8bdc62ee325150a3ba9fab582682431ea043
SamSteele01 <ssteele017@gmail.com>
2018-03-09 14:55:11 -0500

    Add Footer

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,10 @@ import Header from './Header.jsx';
 import Contact from './Contact.jsx';
 import Projects from './Projects.jsx';
 import Toolbelt from './Toolbelt.jsx';
+import Footer from './Footer.jsx';
 import FlipMove from 'react-flip-move';
 // import { Link, DirectLink, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
-import Headroom from 'react-headroom';
+
 
 class App extends Component {
   constructor(props) {
@@ -70,27 +71,19 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App" 
-      // onScroll={this.handleScroll}
-      >
-        {/* {this.state.direction==='top' &&
-          <FlipMove appearAnimation="accordionVertical" leaveAnimation="accordionVertical" maintainContainerHeight={true} >
-            <div> */}
-            <Headroom>
-              <Header displayContact={this.toggleDisplay}/>
-            </Headroom>
-            {/* </div>
-          </FlipMove> */}
-        {/* } */}
+      <div className="App">
+        <Header displayContact={this.toggleDisplay}/>
         <FlipMove enterAnimation="accordionVertical" leaveAnimation="accordionVertical">
           {this.state.displayContact &&
             <Contact/>
           }
         </FlipMove>
-        <Projects 
-        // onScroll={this.handleScroll}
-        />
+        <Projects/>
         <Toolbelt/>
+        <div className='footer app-bottom'>
+          <span>This site was made using React. See the code on gitHub.</span>    
+        </div>
+        <Footer/>
       </div>
     );
   }
