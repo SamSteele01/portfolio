9cf73a15074bf3ba752feb871fa3fa5e911264c8
SamSteele01 <ssteele017@gmail.com>
2018-03-12 17:03:51 -0400

    Add link to gitHub

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Headroom from 'react-headroom';
 
+
 export default class Footer extends Component {
     render() {
         return (
             <div className='footer-wrapper'>
                 <Headroom disableInlineStyles={true} wrapperStyle={{height: 0}}>
                     <div className='footer'>
-                        <span>This site was made using React. See the code on gitHub.</span>    
+                        <span>This site was made using React. <a href="https://github.com/SamSteele01/portfolio" target="_blank">See the code on gitHub.</a></span>    
                     </div>
                 </Headroom>
             </div>
