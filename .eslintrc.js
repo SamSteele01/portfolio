c501e492266eb1606c8aaf63d8254b941de6db59
SamSteele01 <ssteele017@gmail.com>
2019-01-17 18:12:55 -0500

    add prettier and update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,91 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
+  env: {
+    es6: true,
+    browser: true,
+  },
+  extends: [
+    'airbnb',
+    // 'plugin:jest/recommended',
+    // 'jest-enzyme',
+    'eslint:recommended',
+    // 'plugin:prettier/recommended',
+    'prettier/react',
+    'prettier/standard',
+    // 'react-app'
+  ],
+  plugins: [
+    // 'babel',
+    'import',
+    'jsx-a11y',
+    'react',
+    'prettier',
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
+  },
+  rules: {
+    'linebreak-style': 'off', // Don't play nicely with Windows.
+    'arrow-parens': 'off', // Incompatible with prettier
+    'object-curly-newline': 'off', // Incompatible with prettier
+    'no-mixed-operators': 'off', // Incompatible with prettier
+    'arrow-body-style': 'off', // Not our taste?
+    'function-paren-newline': 'off', // Incompatible with prettier
+    'no-plusplus': 'off',
+    'space-before-function-paren': 0, // Incompatible with prettier
+    'for-direction': 'off', // eslint 5.0.0
+    'getter-return': 'off', // eslint 5.0.0
+    'max-len': ['error', 100, 2, { ignoreUrls: true }], // airbnb is allowing some edge cases
+    'no-console': 'off', // airbnb is using warn
+    'no-alert': 'error', // airbnb is using warn
+    'no-param-reassign': 'off', // Not our taste?
+    'no-unused-vars': 'warn',
+    radix: 'off', // parseInt, parseFloat radix turned off. Not my taste.
+
+    'react/require-default-props': 'off', // airbnb use error
+    'react/forbid-prop-types': 'off', // airbnb use error
+    'react/jsx-filename-extension': ['off', { extensions: ['.js'] }], // airbnb is using .jsx
+    'react/destructuring-assignment': 'off',
+    'react/no-find-dom-node': 'off', // I don't know
+    'react/no-did-mount-set-state': 'off',
+    'react/no-unused-prop-types': 'off', // Is still buggy
+    'react/jsx-one-expression-per-line': 'off',
+    // 'jsx-a11y/anchor-is-valid': [
+    //   'error',
+    //   { components: ['Link'], specialLink: ['to'] },
+    // ],
+    'jsx-a11y/anchor-is-valid': [
+      'warn',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['noHref', 'invalidHref', 'preferButton'],
+      },
     ],
-    "rules": {
-        "indent": [
-            2,
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+    'jsx-a11y/label-has-for': 'off',
+    // 'jsx-a11y/label-has-for': [
+    //   2,
+    //   {
+    //     components: ['Label'],
+    //     required: {
+    //       every: ['nesting', 'id'],
+    //     },
+    //     allowChildren: false,
+    //   },
+    // ], // for nested label htmlFor error
+    'jsx-a11y/label-has-associated-control': [
+      2,
+      {
+        labelComponents: ['CustomInputLabel'],
+        labelAttributes: ['label'],
+        controlComponents: ['CustomInput'],
+        depth: 3,
+      },
+    ],
+    'prettier/prettier': ['warn'],
+  },
 };
