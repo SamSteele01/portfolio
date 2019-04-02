baa96a54426239bbcd540995c9d5ce002639749d
SamSteele01 <ssteele017@gmail.com>
2019-04-02 15:16:16 -0400

    add and reorder skills

--- src/data/skills.js
+++ src/data/skills.js
@@ -2,60 +2,80 @@ const skills = [
   {
     name: 'HTML',
     // img: '/assets/images/skills/html-hex-sticker.png'
-    img: require('../styles/hex-stickers/html5-hex-sticker.png')
+    img: require('../styles/hex-stickers/html5-hex-sticker.png'),
   },
   {
     name: 'CSS',
-    img: require('../styles/hex-stickers/css-hex-sticker.png')
+    img: require('../styles/hex-stickers/css-hex-sticker.png'),
   },
   {
     name: 'Sass',
-    img: require('../styles/hex-stickers/sass-hex-sticker.png')
+    img: require('../styles/hex-stickers/sass-hex-sticker.png'),
   },
   // less
   {
-    name: 'JavaScript',
-    img: require('../styles/hex-stickers/JS-hex-sticker.png')
+    name: 'Framer X',
+    img: require('../styles/hex-stickers/framerx-hex-sticker.png'),
   },
   {
-    name: 'React',
-    img: require('../styles/hex-stickers/React-hex-sticker.png')
-  },
-  {
-    name: 'Redux',
-    img: require('../styles/hex-stickers/redux-hex-sticker.png')
+    name: 'Affinity',
+    img: require('../styles/hex-stickers/affinity-hex-sticker.png'),
   },
   // {
-  //   name: 'Backbone',
-  //   img: require('../styles/hex-stickers/backbone-hex-sticker.png')
+  //   name: 'Sketch',
+  //   img: require('../styles/hex-stickers/sketch-hex-sticker.png'),
+  // },
+  // {
+  //   name: 'React',
+  //   img: require('../styles/hex-stickers/React-hex-sticker.png'),
   // },
   {
-    name: 'MongoDB',
-    img: require('../styles/hex-stickers/mongoDB-hex-sticker.png')
+    name: 'JavaScript',
+    img: require('../styles/hex-stickers/JS-hex-sticker.png'),
+  },
+  {
+    name: 'Node.js',
+    img: require('../styles/hex-stickers/nodeJS-hex-sticker.png'),
   },
   {
     name: 'Express',
-    img: require('../styles/hex-stickers/expressJS-hex-sticker.png')
+    img: require('../styles/hex-stickers/expressJS-hex-sticker.png'),
+  },
+  {
+    name: 'React',
+    img: require('../styles/hex-stickers/React-hex-sticker.png'),
+  },
+  {
+    name: 'Material UI',
+    img: require('../styles/hex-stickers/materialUI-hex-sticker.png'),
+  },
+  {
+    name: 'Redux',
+    img: require('../styles/hex-stickers/redux-hex-sticker.png'),
+  },
+  {
+    name: 'Postgres',
+    img: require('../styles/hex-stickers/postgres-hex-sticker.png'),
+  },
+  {
+    name: 'MongoDB',
+    img: require('../styles/hex-stickers/mongoDB-hex-sticker.png'),
   },
   // mustache
   {
-    name: 'Node.js',
-    img: require('../styles/hex-stickers/nodeJS-hex-sticker.png')
+    name: 'Redis',
+    img: require('../styles/hex-stickers/redis-hex-sticker.png'),
   },
   {
     name: 'Python',
-    img: require('../styles/hex-stickers/python-hex-sticker.png')
+    img: require('../styles/hex-stickers/python-hex-sticker.png'),
+  },
+  {
+    name: 'D3',
+    img: require('../styles/hex-stickers/D3-hex-sticker.png'),
   },
   // PHP
   // SQL
-  {
-    name: 'Git',
-    img: require('../styles/hex-stickers/git-hex-sticker.png')
-  },
-  // {
-  //   name: 'JQuery',
-  //   img: require('../styles/hex-stickers/jQuery-hex-sticker.png')
-  // },
   // {
   //   name: 'Meteor',
   //   img: require('../styles/hex-stickers/meteor-hex-sticker.png')
@@ -68,56 +88,53 @@ const skills = [
   //   name: 'Neat',
   //   img: require('../styles/hex-stickers/neat-hex-sticker.png')
   // },
-  // {
-  //   name: 'Chai',
-  //   img: require('../styles/hex-stickers/chai-hex-sticker.png')
-  // },
-  // {
-  //   name: 'Mocha',
-  //   img: require('../styles/hex-stickers/mocha-hex-sticker.png')
-  // },
   {
     name: 'Jest',
-    img: require('../styles/hex-stickers/jest-hex-sticker.png')
+    img: require('../styles/hex-stickers/jest-hex-sticker.png'),
   },
   {
-    name: 'D3',
-    img: require('../styles/hex-stickers/D3-hex-sticker.png')
+    name: 'Chai',
+    img: require('../styles/hex-stickers/chai-hex-sticker.png'),
   },
-  // {
-  //   name: 'Stripe',
-  //   img: require('../styles/hex-stickers/stripe-hex-sticker.png')
-  // },
-  // {
-  //   name: 'Kinvey',
-  //   img: require('../styles/hex-stickers/kinvey-hex-sticker.png')
-  // },
-  // {
-  //   name: 'Pug',
-  //   img: require('../styles/hex-stickers/pug-hex-sticker.png')
-  // },
+  {
+    name: 'Mocha',
+    img: require('../styles/hex-stickers/mocha-hex-sticker.png'),
+  },
+
+  {
+    name: 'Ethereum',
+    img: require('../styles/hex-stickers/ethereum-hex-sticker.png'),
+  },
+  {
+    name: 'Solidity',
+    img: require('../styles/hex-stickers/solidity-hex-sticker.png'),
+  },
+  {
+    name: 'Web3',
+    img: require('../styles/hex-stickers/web3js-hex-sticker.png'),
+  },
   // {
   //   name: 'Gulp',
   //   img: require('../styles/hex-stickers/gulp-hex-sticker.png')
   // },
+
   {
-    name: 'webpack',
-    img: require('../styles/hex-stickers/webpack-hex-sticker.png')
+    name: 'Webpack',
+    img: require('../styles/hex-stickers/webpack-hex-sticker.png'),
   },
   {
     name: 'AWS',
-    img: require('../styles/hex-stickers/aws-hex-sticker.png')
+    img: require('../styles/hex-stickers/aws-hex-sticker.png'),
   },
-  // {
-  //   name: 'Swift',
-  //   img: require('../styles/hex-stickers/swift-hex-sticker.png')
-  // },
-  // adobe photoshop and illustrator
   {
-    name: 'Sketch',
-    img: require('../styles/hex-stickers/sketch-hex-sticker.png')
+    name: 'Docker',
+    img: require('../styles/hex-stickers/docker-hex-sticker.png'),
   },
+  {
+    name: 'Git',
+    img: require('../styles/hex-stickers/git-hex-sticker.png'),
+  },
+  // adobe photoshop, illustrator, krita
+];
 
-]
-
-export default skills
+export default skills;
