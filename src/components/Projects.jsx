4f565303155e9c2fa798437a63c8fd6a7b32f09b
SamSteele01 <ssteele017@gmail.com>
2019-04-07 19:34:28 -0400

    move text and links to seperate files. Add CarouselSingles and Singles

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,154 +4,169 @@ import PropTypes from 'prop-types';
 import Hero from './Hero.jsx';
 import Single from './Single.jsx';
 import VideoSingle from './VideoSingle.jsx';
+import CarouselSingle from './CarouselSingle';
 import TitleBar from './TitleBar.jsx';
+
 import heroText from '../data/heroText.js';
 import projectText from '../data/projectText.js';
-import placeholder from '../styles/projectImages/responsive-design-dark-blue.png';
+import { links } from '../data/links';
+
+import blocX from '../styles/projectImages/blocX-demo-screenshot.png';
+import contractVis from '../styles/projectImages/zeppelin-contract-vis.png';
 import Image1 from '../styles/projectImages/EZ-tour.png';
 import Image2 from '../styles/projectImages/slack-overflow.png';
 import Image3 from '../styles/projectImages/etsy.png';
-import Image4 from '../styles/projectImages/Code-snippet2.png';
-import Image5 from '../styles/projectImages/robot-linkedin.png';
+// import Image4 from '../styles/projectImages/Code-snippet2.png';
+// import Image5 from '../styles/projectImages/robot-linkedin.png';
 import Image6 from '../styles/projectImages/logo.png';
 import Image7 from '../styles/projectImages/Payment-gateway.png';
 
+import eosHackathonImage from '../styles/heroImages/eos-sf-pano2.jpg';
+import bufficorn from '../styles/hackathonImages/bufficorn.jpeg';
+import foodTruck from '../styles/hackathonImages/food-truck-qr.jpeg';
+import smsDiagram from '../styles/hackathonImages/SMS-protocol-DB.png';
+import wyoRoom from '../styles/hackathonImages/wyoHack-room.jpg';
+import wyoTable from '../styles/hackathonImages/wyoHackathon-table.jpg';
+import dcc from '../styles/hackathonImages/dcc-diagram.png';
+import wyoPitch from '../styles/hackathonImages/WyoHackathon-final-pitch-1.jpeg';
+import wyoStage from '../styles/hackathonImages/wyohackathon-stage.jpeg';
+
 export default class Projects extends React.Component {
   render() {
-    const strings = {
-      heroImg3: 'heroImg3',
-      videoM4a2:
-        'https://s3.us-east-2.amazonaws.com/samsteelewebdevportfolio/Pitch+Video+R2D1.mov',
-      videoM4a1:
-        'https://s3.us-east-2.amazonaws.com/samsteelewebdevportfolio/trimmed-eztour-presentation2.mp4',
-      videoWebm1:
-        'https://s3.us-east-2.amazonaws.com/samsteelewebdevportfolio/trimmed-eztour-presentation2.webm',
-      // title6: 'Memory game',
-      link1: 'https://eztour.herokuapp.com/',
-      link2: 'https://blooming-shore-88321.herokuapp.com/',
-      link3: 'https://tiy-durham-etsy-project.github.io/Etsy-group-project/',
-      link6: 'https://travel.booklocal.in/',
-      gitHub1: 'https://github.com/SamSteele01/Final_Project',
-      gitHub2:
-        'https://github.com/TIY-Durham-Slackoverflow-project/Slackoverflow',
-      gitHub3:
-        'https://github.com/TIY-Durham-Etsy-project/Etsy-group-project/tree/master',
-      gitHub4: 'https://github.com/SamSteele01/Robot-LinkedIn',
-      gitHub5: 'https://github.com/SamSteele01/Code-snippet-organizer',
-      gitHub6: 'https://github.com/SamSteele01/memory-game',
-      gitHub7: 'https://github.com/SamSteele01/Week-one-project',
-      description1: 'Using GitHub, Trello, and an Agile methodology',
-      category2: 'Back End Projects',
-      description2: '',
-      category3: 'Front End Projects',
-      description3: '',
-    };
-
     return (
       <div className="project">
-        <Hero imageClassName="heroImg1" heroText={heroText[1]} />
-
-        <div className="title-bar">
-          <h2>Blockchain</h2>
-          <p>
-            With all the hype in late 2017, I just had to learn first hand what
-            this blockchain stuff was all about. My biggest takeaways:
-            Blockchain technologies have the potential to add value, but are
-            just another microservice and need to be part of an architecture
-            that can utilize what they have to offer. Smart contracts are easy
-            to write, but there is a lot to consider about security
-            vunerabilities, gas fees, and updating the contracts. That is, you
-            really want to spend the time and get it 100% right before deploying
-            a contract to a mainnet.
-          </p>
-        </div>
+        <Hero imageClassName="blockchain-image" />
+        <TitleBar category="Blockchain" description={projectText.blockchain} />
         <Single
           id={0}
+          title="BlocX Demo Wallet"
+          image={blocX}
+          link="http://blocx-demo-wallet.s3-website-us-east-1.amazonaws.com/"
+          text={projectText.blocX}
+        />
+        <Single
+          id={1}
           title="Cryptocurrency Payment Gateway"
           image={Image7}
-          placeholder={placeholder}
-          text={projectText[7]}
+          text={projectText.vendible}
+          backward={true}
         />
         <VideoSingle
           id={0}
           title="BookLocal"
-          videoM4a={strings.videoM4a2}
-          // videoWebm={strings.videoWebm1}
+          videoM4a={links.videoM4a2}
+          // videoWebm={links.videoWebm1}
           image={Image6}
           hoverMessage="Watch the demo video"
-          placeholder={placeholder}
-          link={strings.link6}
-          text={projectText[6]}
-          backward={true}
+          link={links.link6}
+          text={projectText.booklocal}
         />
 
-        <Hero imageClassName="iron-yard-glass" />
+        <img
+          src={eosHackathonImage}
+          alt="screenshot of application"
+          className="pano-image"
+        />
+
+        <TitleBar category="Hackathons" description="uh ..." />
+        {/* EthDenver */}
+        <CarouselSingle
+          id={0}
+          title="EthDenver 2019"
+          images={[bufficorn, smsDiagram, foodTruck]}
+          text={projectText.ethDenver}
+          backward={true}
+        />
+        {/* WyoHackathon */}
+        <CarouselSingle
+          id={1}
+          title="WyoHackathon 2018"
+          images={[wyoRoom, wyoTable, dcc, wyoPitch, wyoStage]}
+          text={projectText.wyoHackathon}
+          // backward={true}
+        />
+        {/* EthMemphis */}
+
+        <Hero imageClassName="react-code-screen" />
 
         <TitleBar
+          category="Practice Projects"
+          description={projectText.practiceProjects}
+        />
+        {/* contract visualizer */}
+        <Single
+          id={2}
+          title="Contract Visualizer"
+          image={contractVis}
+          text={projectText.contractVis}
+          link={links.contractVis}
+          gitHub={links.contractVisGithub}
+          backward={true}
+        />
+        {/* web assembly */}
+        {/* <Single
+          id={3}
+          title="Fractals in Web Assembly"
+          image={contractVis}
+          text={projectText.fractals}
+          backward={true}
+        /> */}
+
+        <Hero imageClassName="iron-yard-glass" />
+        <TitleBar
           category="The Iron Yard"
-          description="The Iron Yard is a 12-week coding boot camp specializing
-            in highly focused, immersive training centered on language fluency,
-            object-oriented programming, and project-based learning. Team
-            projects using GitHub, Trello, and an Agile methodology. There was a
-             final project that took an additional month. My two biggest
-             takaways were learning how to Google my way through any problem and
-              getting use to writing code for 60 hours a week."
+          description={projectText.theIronYard}
         />
         <VideoSingle
           id={1}
           title="EZ-Tour"
-          videoM4a={strings.videoM4a1}
-          videoWebm={strings.videoWebm1}
+          videoM4a={links.videoM4a1}
+          videoWebm={links.videoWebm1}
           image={Image1}
           hoverMessage="See our presentation from demo day"
-          placeholder={placeholder}
-          link={strings.link1}
-          text={projectText[1]}
-          gitHub={strings.gitHub1}
+          link={links.link1}
+          text={projectText.ezTour}
+          gitHub={links.gitHub1}
         />
         <Single
-          id={1}
+          id={3}
           title="Slack-overflow"
           image={Image2}
-          placeholder={placeholder}
-          link={strings.link2}
-          text={projectText[2]}
-          gitHub={strings.gitHub2}
+          link={links.link2}
+          text={projectText.slackOverflow}
+          gitHub={links.gitHub2}
           backward={true}
         />
         <Single
-          id={2}
+          id={4}
           title="Etsy"
           image={Image3}
-          placeholder={placeholder}
-          link={strings.link3}
-          text={projectText[3]}
-          gitHub={strings.gitHub3}
+          link={links.link3}
+          text={projectText.etsy}
+          gitHub={links.gitHub3}
         />
 
-        <Hero imageClassName="heroImg2" />
+        {/* <Hero imageClassName="heroImg2" />
 
         <TitleBar
           category="Back End Projects"
-          description={strings.description2}
+          description={links.description2}
         />
         <Single
           id={3}
           title="Code snippet organizer"
           image={Image4}
-          placeholder={placeholder}
           text={projectText[4]}
-          gitHub={strings.gitHub4}
+          gitHub={links.gitHub4}
           backward={true}
         />
         <Single
           id={4}
           title="LinkedIn for Robots"
           image={Image5}
-          placeholder={placeholder}
           text={projectText[5]}
-          gitHub={strings.gitHub5}
-        />
+          gitHub={links.gitHub5}
+        /> */}
       </div>
     );
   }
