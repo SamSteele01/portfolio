98b974da4bef174c66b14de14317c12fd8c06f18
SamSteele01 <ssteele017@gmail.com>
2019-04-07 19:30:54 -0400

    move scroll listener to Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,89 +1,22 @@
 import React, { Component } from 'react';
-import '../styles/App.css';
 import Header from './Header.jsx';
-import Contact from './Contact.jsx';
 import Projects from './Projects.jsx';
 import Toolbelt from './Toolbelt.jsx';
 import Footer from './Footer.jsx';
-import FlipMove from 'react-flip-move';
-// import { Link, DirectLink, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
 
+import '../styles/App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.toggleDisplay = this.toggleDisplay.bind(this);
-    // this.handleScroll = this.handleScroll.bind(this);
-
-    this.state = {
-      displayContact: false,
-      direction:'top',
-      lastScrollPos:0
-    };
-  }
-
-  // componentDidMount() {
-  //     window.addEventListener('scroll', this.handleScroll);
-  // }
-
-  // componentWillUnmount() {
-  //     window.removeEventListener('scroll', this.handleScroll);
-  // }
-
-  // componentDidMount() {
-  //   Events.scrollEvent.register('begin', function(to, element) {
-  //     console.log("begin", arguments);
-  //   });
-  //   Events.scrollEvent.register('end', function(to, element) {
-  //     console.log("end", arguments);
-  //   });
-  // }
-
-  // componentWillUnmount() {
-  //   Events.scrollEvent.remove('begin');
-  //   Events.scrollEvent.remove('end');
-  // }
-
-  // handleScroll(event) {
-  //   console.log('scrollY', window.scrollY);
-  //   console.log('scrollTop', window.scrollTop);
-  //   if(this.state.lastScrollPos > scroll ) {
-  //     this.setState({
-  //       direction:'top',
-  //       lastScrollPos:window.scrollY
-  //     });
-  //   }
-  //   if(this.state.lastScrollPos < scroll ) {
-  //     this.setState({
-  //       direction:'bottom',
-  //       lastScrollPos:window.scrollY
-  //     });
-  //   }
-    // this.setState({
-    //   lastScrollPos:window.scrollY
-    // });
-  // }
-
-  toggleDisplay(){
-    this.setState({displayContact: !this.state.displayContact});
-  }
-
   render() {
     return (
       <div className="App">
-        <Header displayContact={this.toggleDisplay}/>
-        <FlipMove enterAnimation="accordionVertical" leaveAnimation="accordionVertical">
-          {this.state.displayContact &&
-            <Contact/>
-          }
-        </FlipMove>
-        <Projects/>
-        <Toolbelt/>
-        <div className='footer app-bottom'>
-          <span>This site was made using React. See the code on gitHub.</span>    
+        <Header />
+        <Projects />
+        <Toolbelt />
+        <div className="footer app-bottom">
+          <span>This site was made using React. See the code on gitHub.</span>
         </div>
-        <Footer/>
+        <Footer />
       </div>
     );
   }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,65 @@
 import React, { Component } from 'react';
-import logo from '../styles/Logo.png';
 import Headroom from 'react-headroom';
+import FlipMove from 'react-flip-move';
+
+import Contact from './Contact.jsx';
+import logo from '../styles/Logo.png';
 
 export default class Header extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      displayContact: false,
+      lastScrollPos: 0,
+    };
+    this.handleScroll = this.handleScroll.bind(this);
   }
 
-  // want to have a contact button
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll(event) {
+    if (this.state.lastScrollPos < window.scrollY) {
+      this.setState({
+        displayContact: false,
+      });
+    }
+    this.setState({
+      lastScrollPos: window.scrollY,
+    });
+  }
+
+  toggleDisplay = () => {
+    this.setState({ displayContact: !this.state.displayContact });
+  };
+
   render() {
     return (
       <div className="header-wrapper">
         <Headroom disableInlineStyles={true}>
           <header className="App-header">
-            {/* <div className="take-Up-Space"></div> */}
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="pixeled-text ">Portfolio</h1>
-            <button
-              className="contact-button"
-              onClick={this.props.displayContact}
+            <h1
+            // className="pixeled-text "
             >
+              Portfolio
+            </h1>
+            <button className="contact-button" onClick={this.toggleDisplay}>
               Contact
             </button>
           </header>
+          <FlipMove
+            enterAnimation="accordionVertical"
+            leaveAnimation="accordionVertical"
+          >
+            {this.state.displayContact && <Contact />}
+          </FlipMove>
         </Headroom>
       </div>
     );
